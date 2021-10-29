b448ed6e29866954d037ab1d53adad41cbc5a9ec
Gatis Ķevans <gatis.kevans@gmail.com>
2021-10-29 20:59:05 +0300

    Update RegisterController.php

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -39,8 +39,6 @@ class RegisterController extends Controller
 
         auth()->login($user);   //Logs user in
 
-        //session()->flash('success', 'Your account has been created.');
-
         return redirect('/')->with('success', 'Your account has been created.');
     }
 }
